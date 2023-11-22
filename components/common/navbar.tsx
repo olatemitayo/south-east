73c6261217c2eb664a3c31ba07758bc39dd25f0b
olatemitayo <olatunjitemitayo444@gmail.com>
2023-11-22 17:11:52 +0100

    new files

--- components/common/navbar.tsx
+++ components/common/navbar.tsx
@@ -1,113 +1,39 @@
-import Link from "next/link";
-import clsx from "clsx";
-import { useDisclosure } from "@mantine/hooks";
-import { Burger, Flex, Popover } from "@mantine/core";
-import { useRouter } from "next/router";
-import Icons from "./navbarIcons";
-import useCartProduct from "@/hooks/use-cart-list";
-import MobileIcons from "./mobileIcons";
-const navList = [
-  {
-    id: 1,
-    title: "HOME",
-    link: "/",
-  },
-  {
-    id: 2,
-    title: "SHOP",
-    link: "/products",
-  },
-  {
-    id: 3,
-    title: "ABOUT US",
-    link: "#",
-  },
-  {
-    id: 4,
-    title: "CONTACT US",
-    link: "#",
-  },
-];
+import { Box, Flex, Popover, Text } from "@mantine/core";
+import React from "react";
 
-// mobileNavL;
-
-interface NavbarProps {
-  className?: string;
-}
-
-export default function Navbar({ className }: NavbarProps) {
-  const { data } = useCartProduct();
-  const router = useRouter();
-  const [opened, { toggle }] = useDisclosure(false);
-  const label = opened ? "Close navigation" : "Open navigation";
-
+export function Navbar() {
   return (
-    <div
-      className={clsx(
-        "fixed w-full z-10 backdrop-blur-md bg-white/50",
-        className
-      )}
-    >
-      <nav className="flex justify-between px-[clamp(10px,6vw,100px)]  items-center py-4 max-w-[1440px] mx-auto">
-        <div className="flex gap-[clamp(30px,5vw,80px)] items-center">
-          <img
-            src="/logo.svg"
-            alt="LOGO"
-            className="w-[clamp(60px,6vw,95px)]"
-          />
-          <ul className="flex  gap-[clamp(20px,4vw,56px)] cmd:hidden">
-            {navList.map((item) => (
-              <Link key={item.id} href={item.link}>
-                <li
-                  key={item.id}
-                  className={`${
-                    router.pathname === item.link ||
-                    router.pathname === `${item.link}`
-                      ? "flex text-[clamp(14px,1vw,16px)] flex-nowrap text-[#771132] underline"
-                      : "text-[#121212] flex text-[clamp(14px,1vw,16px)] flex-nowrap"
-                  }  `}
-                >
-                  {item.title}
-                </li>
-              </Link>
-            ))}
-          </ul>
-        </div>
-        <div>
-          <Icons />
-          <div className="hidden cmd:block">
-            <Popover width={180} position="bottom" withArrow shadow="md">
-              <Flex className="items-center" gap={"25px"}>
-                <Flex>
-                  <MobileIcons className="cmd:!flex" />
-                </Flex>
-                <Popover.Target>
-                  <Burger
-                    opened={opened}
-                    onClick={toggle}
-                    aria-label={label}
-                    color="#6B0F2D"
-                  />
-                </Popover.Target>
-              </Flex>
-              <Popover.Dropdown>
-                <ul className="flex  flex-col gap-4">
-                  {navList.map((item) => (
-                    <Link key={item.id} href={item.link} className="flex ">
-                      <li
-                        key={item.id}
-                        className="flex text-[12px] flex-nowrap hover:text-[#771132] hover:underline text-[#121212]"
-                      >
-                        {item.title}
-                      </li>
-                    </Link>
-                  ))}
-                </ul>
-              </Popover.Dropdown>
-            </Popover>
-          </div>
-        </div>
-      </nav>
-    </div>
+    <Box bg="#b44f21">
+      <Flex
+        justify="space-between"
+        align="center"
+        w="100%"
+        maw={1330}
+        mx="auto"
+        p={16}
+      >
+        <img src="/oyo-state-logo.png" alt="LOGO" />
+        <Flex gap={32} c="white">
+          <Text>Home</Text>
+          <Text>News</Text>
+          <Text>Gallery</Text>
+        </Flex>
+
+        <Popover width={200} position="bottom" shadow="md">
+          <Popover.Target>
+            <Box bg="#ED6600">
+              <Text c="white" p={8}>
+                Contact Us
+              </Text>
+            </Box>
+          </Popover.Target>
+          <Popover.Dropdown>
+            <Text size="sm">
+              This is uncontrolled popover, it is opened when button is clicked
+            </Text>
+          </Popover.Dropdown>
+        </Popover>
+      </Flex>
+    </Box>
   );
 }
